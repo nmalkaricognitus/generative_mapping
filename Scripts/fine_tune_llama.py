b72da2714f98cc4755d9490e18eff4c28a1c27ae
nmalkaricognitus <nmalkari@cognitus.com>
2023-10-23 20:51:44 -0400

    test

--- Scripts/fine_tune_llama.py
+++ Scripts/fine_tune_llama.py
@@ -1,23 +1,28 @@
 import torch
+import transformers
 from torch.utils.data import DataLoader
 from transformers import BertTokenizer, BertForSequenceClassification
-from my_dataset import SpiderDataset  # Assume SpiderDataset is a custom dataset class you've defined
+from my_dataset import SpiderDataset 
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from huggingface_hub import hf_hub_download
+from huggingface_hub import login
 
-# Load the Spider dataset
-dataset = SpiderDataset('path_to_spider_dataset')
+
+
+dataset = SpiderDataset('/Users/nikhilmalkari/Documents/Cognitus/GenerativeMapping/generative_mapping/spider')
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
-# Load a pre-trained model
+
 model = "openlm-research/open_llama_7b_v2" 
 
 tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
 
-# Define the optimizer, loss function, etc.
+
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 loss_fn = torch.nn.CrossEntropyLoss()
 
-# Training loop
-for epoch in range(3):  # Assume 3 epochs for simplicity
+
+for epoch in range(3):
     for batch in dataloader:
         inputs = tokenizer(batch['text'], padding=True, truncation=True, return_tensors='pt')
         outputs = model(**inputs, labels=batch['labels'])
@@ -28,5 +33,6 @@ for epoch in range(3):  # Assume 3 epochs for simplicity
 
     print(f'Epoch {epoch+1}, Loss: {loss.item()}')
 
-# Save the fine-tuned model
+
 model.save_pretrained('path_to_save_model')
+
