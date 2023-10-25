271cde0a4b52d15a290e6bf38641d68f77ab4430
nmalkaricognitus <nmalkari@cognitus.com>
2023-10-25 16:27:04 -0400

    tune30

--- Scripts/already_fine.py
+++ Scripts/already_fine.py
@@ -19,39 +19,65 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = T5ForConditionalGeneration.from_pretrained('cssupport/t5-small-awesome-text-to-sql')
 model = model.to(device)
 
-pipeline = pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device=0,
-    max_length=10000,
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-)
+model.eval()
 
-llm = HuggingFacePipeline(pipeline=pipeline, model_kwargs={'temperature': 0.7})
+def generate_sql(input_prompt):
+    # Tokenize the input prompt
+    inputs = tokenizer(input_prompt, padding=True, truncation=True, return_tensors="pt").to(device)
+    
+    # Forward pass
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_length=512)
+    
+    # Decode the output IDs to a string (SQL query in this case)
+    generated_sql = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    
+    return generated_sql
 
-# Create a SQL database object
-db_user = "cognitus"
-db_password = "student"
-db_host = "localhost"
-db_name = "generative_mapping"
-db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
+# Test the function
+#input_prompt = "tables:\n" + "CREATE TABLE Catalogs (date_of_latest_revision VARCHAR)" + "\n" +"query for: Find the dates on which more than one revisions were made."
+#input_prompt = "tables:\n" + "CREATE TABLE table_22767 ( \"Year\" real, \"World\" real, \"Asia\" text, \"Africa\" text, \"Europe\" text, \"Latin America/Caribbean\" text, \"Northern America\" text, \"Oceania\" text )" + "\n" +"query for:what will the population of Asia be when Latin America/Caribbean is 783 (7.5%)?."
+#input_prompt = "tables:\n" + "CREATE TABLE procedures ( subject_id text, hadm_id text, icd9_code text, short_title text, long_title text ) CREATE TABLE diagnoses ( subject_id text, hadm_id text, icd9_code text, short_title text, long_title text ) CREATE TABLE lab ( subject_id text, hadm_id text, itemid text, charttime text, flag text, value_unit text, label text, fluid text ) CREATE TABLE demographic ( subject_id text, hadm_id text, name text, marital_status text, age text, dob text, gender text, language text, religion text, admission_type text, days_stay text, insurance text, ethnicity text, expire_flag text, admission_location text, discharge_location text, diagnosis text, dod text, dob_year text, dod_year text, admittime text, dischtime text, admityear text ) CREATE TABLE prescriptions ( subject_id text, hadm_id text, icustay_id text, drug_type text, drug text, formulary_drug_cd text, route text, drug_dose text )" + "\n" +"query for:" + "what is the total number of patients who were diagnosed with icd9 code 2254?"
+input_prompt = "How many tables are there in the database ?"
+
+generated_sql = generate_sql(input_prompt)
+
+print(f"The generated SQL query is: {generated_sql}")
 
 
-toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+# pipeline = pipeline(
+#     "text-generation",
+#     model=model,
+#     tokenizer=tokenizer,
+#     torch_dtype=torch.bfloat16,
+#     trust_remote_code=True,
+#     device=0,
+#     max_length=10000,
+#     do_sample=True,
+#     top_k=10,
+#     num_return_sequences=1,
+#     eos_token_id=tokenizer.eos_token_id,
+# )
 
-agent_executor = create_sql_agent(
-        llm=llm,
-        toolkit=toolkit,
-        verbose=True)
-bot_response = agent_executor.run("What is the total number of tables present in the database")
+# llm = HuggingFacePipeline(pipeline=pipeline, model_kwargs={'temperature': 0.7})
 
-print(bot_response)
+# # Create a SQL database object
+# db_user = "cognitus"
+# db_password = "student"
+# db_host = "localhost"
+# db_name = "generative_mapping"
+# db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
+
+
+# toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+# agent_executor = create_sql_agent(
+#         llm=llm,
+#         toolkit=toolkit,
+#         verbose=True)
+# bot_response = agent_executor.run("What is the total number of tables present in the database")
+
+# print(bot_response)
 
 # # Create the agent executor
 # agent_executor = langchain.AgentExecutor(llm=llm, db=db, verbose=True)
